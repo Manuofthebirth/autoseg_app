4a0b0b3a74d712742238fa9b808ba297c4d66ae4
Manoel do Nascimento <manu3105@yahoo.com>
2019-05-24 10:46:22 -0300

    Show task feature added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
 
   def index
-    @tasks = Task.all.where(isprivate: false)
-    # @tasks = Task.where(:user_id => current_user.id)
+    # @tasks = Task.all.where(isprivate: false)
+    @tasks = Task.where(:user_id => current_user.id)
+  end
+
+  def show
   end
 
   def new
@@ -22,5 +26,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :description, :isprivate)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
