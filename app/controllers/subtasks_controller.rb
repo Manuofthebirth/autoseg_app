958eaa51816c3e46156aab81617dbeb5185bec05
Manoel do Nascimento <manu3105@yahoo.com>
2019-05-24 16:09:43 -0300

    Subtask features added

--- app/controllers/subtasks_controller.rb
+++ app/controllers/subtasks_controller.rb
@@ -1,2 +1,28 @@
 class SubtasksController < ApplicationController
+  before_action :find_task
+  before_action :find_subtask, except: [:create]
+
+  def create
+    @subtask = @task.subtasks.create(subtask_params)
+    redirect_to @task
+  end
+
+  def destroy
+    @subtask.destroy
+    redirect_to @task
+  end
+
+  private
+
+  def subtask_params
+    params[:subtask].permit(:title, :deadline_date)
+  end
+
+  def find_task
+    @task = TodoList.find(params[:todo_list_id])
+  end
+
+  def find_subtask
+    @subtask = @task.todo_items.find(params[:id])
+  end
 end
