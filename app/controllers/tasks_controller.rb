b6254a83ed4bed8e1ce281eec68dba583f2bebb1
Manoel do Nascimento <manu3105@yahoo.com>
2019-05-24 01:11:40 -0300

    Index page + Create feature added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,26 @@
 class TasksController < ApplicationController
-  
+
   def index
     @tasks = Task.all.where(isprivate: false)
+    # @tasks = Task.where(:user_id => current_user.id)
   end
 
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :isprivate)
+  end
 end
